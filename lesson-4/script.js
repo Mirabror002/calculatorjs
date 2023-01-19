4aa64c744a858ea10b4370b9fef0a9b586c1d901
Mirabror Rashidov [FE-17] <122270982+Mirabror002@users.noreply.github.com>
2023-01-19 22:29:43 +0500

    Add files via upload

---
+++ lesson-4/script.js
@@ -0,0 +1,13 @@
+let num =+prompt("Birinchi son?")
+let num2 =prompt("Amal")
+let num3 =+prompt("Ikkinchi son?")
+ if(num2=="+"){
+    console.log(num+num3)
+ }else if(num2=="-"){
+     console.log(num-num3)
+ }
+  else if(num2=="*"){
+    console.log(num*num3)
+ }else if(num2=="/"){
+     console.log(num/num3)
+ }
